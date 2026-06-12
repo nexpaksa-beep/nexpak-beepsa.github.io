305936df175942a1bf6b1b83f9b15f392863163e
nexpaksa-beep <nexpaksa@gmail.com>
2026-06-12 14:15:34 +0200

    Update shop-data.js

--- shop-data.js
+++ shop-data.js
@@ -19,8 +19,8 @@ const products = [
         description: "Protective bubble wrap for fragile goods and courier packaging.",
         features: ["Lightweight", "Shock absorbing", "Industrial grade"],
         images: [
-            "/bubblew1.jpeg",
-            "/bubblew2.jpeg",
+            "products/bubblew1.jpeg",
+            "bubblew2.jpeg",
             "/bubblew3.jpeg",
             "/bubblew4.jpeg"
         ],
