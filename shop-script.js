9252d68679a582d50c9bedc4e29d9d8ab0fd3b71
nexpaksa-beep <nexpaksa@gmail.com>
2026-07-04 11:12:49 +0200

    Update shop-script.js

--- shop-script.js
+++ shop-script.js
@@ -316,7 +316,7 @@ async function processOrder() {
 
     try {
 
-        const res = await fetch("http://localhost:4242/create-checkout-session", {
+        const res = await fetch("http://nexpak-backend-2.onrender.com/create-checkout-session", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ items: cart })
