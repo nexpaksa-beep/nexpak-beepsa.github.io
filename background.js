6b6f3c0648c503125010ebd3f5971b0004c8e58f
nexpaksa-beep <nexpaksa@gmail.com>
2026-08-05 21:14:50 +0200

    Update background.js

--- background.js
+++ background.js
@@ -6,18 +6,38 @@ const images = [
     "images/hero5.png"
 ];
 
-let index = 0;
+document.addEventListener("DOMContentLoaded", () => {
 
-function startBackgroundSlider() {
+    const bg1 = document.querySelector(".hero-bg1");
+    const bg2 = document.querySelector(".hero-bg2");
 
-    document.body.style.backgroundImage = `url('${images[0]}')`;
+    let current = 0;
+    let showingFirst = true;
+
+    bg1.style.backgroundImage = `url('${images[0]}')`;
+    bg2.style.backgroundImage = `url('${images[1]}')`;
 
     setInterval(() => {
 
-        index = (index + 1) % images.length;
-        document.body.style.backgroundImage = `url('${images[index]}')`;
+        current = (current + 1) % images.length;
+        const next = (current + 1) % images.length;
+
+        if (showingFirst) {
+
+            bg2.style.backgroundImage = `url('${images[next]}')`;
+            bg2.style.opacity = "1";
+            bg1.style.opacity = "0";
+
+        } else {
+
+            bg1.style.backgroundImage = `url('${images[next]}')`;
+            bg1.style.opacity = "1";
+            bg2.style.opacity = "0";
+
+        }
+
+        showingFirst = !showingFirst;
 
     }, 5000);
-}
 
-document.addEventListener("DOMContentLoaded", startBackgroundSlider);
+});
